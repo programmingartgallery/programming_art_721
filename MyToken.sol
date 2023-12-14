f1f9806f35e62cd77783f78ea5835462e2c82708
programmingartgallery <153808433+programmingartgallery@users.noreply.github.com>
2023-12-14 11:14:10 +0100

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "./contracts/token/ERC721/IERC721.sol";
 
 /// @title A simulator for trees
 /// @author Larry A. Gardner
