fee9fea07781d64e1ee18db76f41c80a0a1a521d
programmingartgallery <153808433+programmingartgallery@users.noreply.github.com>
2023-12-14 10:50:58 +0100

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.2 < 0.9.0;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-/**
- * @title MyToken contract
- * @custom:security-contact security@example.com
- */
 /// @title A simulator for trees
 /// @author Larry A. Gardner
 /// @notice You can use this contract for only the most basic simulation
